2ddea57fb84800030bf6051ffb733bd8a359cca5
Clay Sheppard <17840614+csheepy@users.noreply.github.com>
2025-01-02 21:37:51 -0600

    minion data updates

--- module/data/actor-minion.mjs
+++ module/data/actor-minion.mjs
@@ -7,9 +7,8 @@ export default class ProwlersParagonsMinion extends ProwlersParagonsDataModel {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = {};
 
-    schema.threat = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 2, min: 0 }),
-    });
+    schema.threat = new fields.NumberField({ ...requiredInteger, initial: 2, min: 0 });
+    schema.count = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 }); // number of minions in a group
 
     schema.description = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
