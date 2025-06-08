a4c1c220e45df8e0b725c6b7c79ec9824c0d09d4
Clay Sheppard <17840614+csheepy@users.noreply.github.com>
2025-06-08 14:50:43 -0500

    add 'resolve' as a potential attribute bar for tokens

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -34,6 +34,7 @@ export default class ProwlersParagonsCharacter extends ProwlersParagonsActorBase
     schema.resolve = new fields.SchemaField({
       value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
       starting: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 })
     });
 
     schema.biography = new fields.SchemaField({
@@ -93,6 +94,8 @@ export default class ProwlersParagonsCharacter extends ProwlersParagonsActorBase
         this.edge.value = Math.max(this.edge.value,superSpeed.system.rank * 3)
       }
     }
+
+    this.resolve.max = this.resolve.starting;
   }
 
   getRollData() {
