832068e56c89d4d7db8a6b7ef5082eed17f32f10
Clay Sheppard <17840614+csheepy@users.noreply.github.com>
2025-06-18 18:24:59 -0500

    fix bug with minion description

--- module/data/actor-minion.mjs
+++ module/data/actor-minion.mjs
@@ -10,9 +10,19 @@ export default class ProwlersParagonsMinion extends ProwlersParagonsDataModel {
     schema.threat = new fields.NumberField({ ...requiredInteger, initial: 2, min: 0 });
     schema.count = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 }); // number of minions in a group
 
-    schema.description = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
+    schema.biography = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
     return schema;
+  }
+
+  /**
+   * Migrate source data from some prior format into the format required by this model.
+   * @param {object} source   The source data, which may be in an old format
+   */
+  static migrateData(source) {
+    // 1.5.0: Migrate description to biography
+    foundry.abstract.Document._addDataFieldMigration(source, "description", "biography");
+    return super.migrateData(source);
   }
 
 }
